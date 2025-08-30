57484099c35ca499a961b6428aa3746aa647e78c
BURAK KAAN ÖZDEMİR <ozdemirburakkaan@gmail.com>
2025-08-30 23:18:12 +0300

    Create 2.4-BasicMethod.java

---
+++ 2.4-BasicMethod.java
@@ -0,0 +1,10 @@
+public class BasicMethod {
+    // This method prints a greeting message
+    public static void greet() { // If you prefer to not return anything we must write "void" after the "static"
+        System.out.println("Hello! Welcome to Java Methods. My name is Burak Kaan Özdemir! ");
+    }
+
+    public static void main(String[] args) {
+        greet(); // call the method
+    }
+}
