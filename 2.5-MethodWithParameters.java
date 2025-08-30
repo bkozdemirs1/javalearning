c1fbe097c351757400268a83bd96635ab0876f3f
BURAK KAAN ÖZDEMİR <ozdemirburakkaan@gmail.com>
2025-08-30 23:18:34 +0300

    Create 2.5-MethodWithParameters.java

---
+++ 2.5-MethodWithParameters.java
@@ -0,0 +1,10 @@
+public class MethodWithParameters {
+    // This method takes two integers and prints their sum
+    public static void sum(int a, int b) {
+        System.out.println("Sum: " + (a + b));
+    }
+
+    public static void main(String[] args) {
+        sum(5, 10); // call method with arguments
+    }
+}
