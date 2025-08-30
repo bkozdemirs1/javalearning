d3e4a8ca1967561d803a0ecb8ae0c615c165b0ab
BURAK KAAN ÖZDEMİR <ozdemirburakkaan@gmail.com>
2025-08-30 21:06:41 +0300

    Create 2.1-StringLength.java

---
+++ 2.1-StringLength.java
@@ -0,0 +1,9 @@
+public class StringLength {
+    
+  public static void main(String[] args) {
+        String text = "Java Learning";
+        
+        // length() → counts total characters in the string
+        System.out.println("Length: " + text.length());
+    }
+}
